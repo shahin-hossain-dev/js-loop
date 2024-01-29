dd3f5bec0a3e854aa3f3a0647f78d907e580b0ae
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-29 20:28:05 +0600

    18_2 Introduction To While Loop

---
+++ while.js
@@ -0,0 +1,22 @@
+/**
+ * *******************
+ *   WHILE LOOP
+ * *******************
+ *  */
+// if-else এবং loop এর পার্থ্ক্য হলো if-else একবার check করে শেষ হয়ে যায় 
+// কিন্তু লুপ হলো বার বার চেক করতে থাকে যতক্ষন পর্যন্ত condition সত্য হয়। condition false হলে loop stop হয়ে যাবে।।
+/* const number = 7;
+
+if (number < 5) {
+    console.log('choto akta number paisi')
+}
+console.log('something new is cooking') */
+
+let num = 0; //loop variable 
+
+while (num < 7) {
+    console.log('Looping is running', num)
+    // num = num + 1;
+    // num += 1;
+    num++; //++ double plus দিয়ে num এর value ১ করে increment হবে।
+}
