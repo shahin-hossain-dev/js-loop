afeb78a1d16b70461e8f35bd64d22630806ada9b
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-30 17:26:54 +0600

    18_8 (Optional) Introduction To Do While Loop

---
+++ do-while.js
@@ -0,0 +1,17 @@
+/**
+ * *******************
+ *   DO WHILE LOOP
+ * *******************
+ * ata almost use korbo na
+ *   */
+
+// কিভাবে কাজ করে
+// do-while এর প্রথম বৈশিষ্ট্য হলো do এর মধ্যে যা থাকবে সেটা একবার execute হবে তারপর condition check করবে।
+// সাধারণ while এ আগে condition চেক করবে তারপর execute হবে।
+
+let n = 10;
+do {
+    console.log(n);
+    n++;
+}
+while (n < 5);
